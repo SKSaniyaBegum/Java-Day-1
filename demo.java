a859b257906451c17a533cf9f83a27c32f5fd456
SKSaniyaBegum <sksaniyabegum06@gmail.com>
2026-01-31 14:28:53 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,13 @@
+public class Demo {
+    public static void main(String[] args)
+    {
+        int a=89;
+        System.out.println(a);
+        a=99;
+        System.out.print(a);
+    System.out.println("Welcome to java class");
+    System.out.printf("Welcome to club");
+    System.out.println("Welcome");
+    }   
+}
+
